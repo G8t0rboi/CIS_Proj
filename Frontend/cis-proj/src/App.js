0fed416b023ea04b526dc7b51fd96ca754f2f86c
g8t0rboi <g8t0rboispam@gmail.com>
2023-04-17 16:51:22 -0400

    Added React-chartjs-2 as Dependency Fixed data formatting

--- Frontend/cis-proj/src/App.js
+++ Frontend/cis-proj/src/App.js
@@ -3,7 +3,6 @@ import NavbarComp from './components/NavbarComp';
 import React, {useEffect, useState} from 'react';
 import axios from 'axios'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { LineChartComp } from './components/LineChartComp';
 import Button from 'react-bootstrap/Button'
 
 function App() {
@@ -14,28 +13,18 @@ function App() {
 
     axios.get("http://localhost:8080/students").then((response) => {
 
-      console.log(response)
+      console.log(response.data.rows[0].YEAR)
 
     })
+    
 
   }
-
-  useEffect(() => {
-
-    axios.get("http://localhost:8080/message").then((response) => {
-
-      console.log(response.data);
-
-    })
-
-  }, [])
   
   return (
     <>
       <NavbarComp></NavbarComp>
       <Button onClick={getData}>Get Data</Button>
-      <p>{data}</p>
-
+      
     </>
   );
 }
