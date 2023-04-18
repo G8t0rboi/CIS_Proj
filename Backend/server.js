e0112774b33d3f1595c38a0b6008e9870c2a9159
g8t0rboi <g8t0rboispam@gmail.com>
2023-04-18 13:21:32 -0400

    Fixed Post Requests

--- Backend/server.js
+++ Backend/server.js
@@ -5,6 +5,13 @@ const cors = require('cors');
 const app = express();
 
 app.use(cors());
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }));
+
+var x;
+var y;
+var z;
+var h;
 
 app.get('/students', (req, res) => {
 
@@ -14,7 +21,7 @@ app.get('/students', (req, res) => {
 
             const connection = await oracledb.getConnection({
 
-                user: "manuel.nunez", 
+                user: "manuel.nunez",
                 password: "SJ3vtvEHEFavwAGrAwjUQ2XT",
                 connectString: "(DESCRIPTION =(ADDRESS = (PROTOCOL = TCP)(HOST = oracle.cise.ufl.edu)(PORT = 1521))(CONNECT_DATA =(SID= ORCL)))"
             })
@@ -42,195 +49,245 @@ app.get('/students', (req, res) => {
 
         })
 
+    res.end;
+})
+
+app.post("/results", (req, res) => {
+
+    const body = req.body;
+    console.log(body);
+
+    async function fetchDataStudents() {
+
+        try {
+
+            const connection = await oracledb.getConnection({
+
+                user: "manuel.nunez",
+                password: "SJ3vtvEHEFavwAGrAwjUQ2XT",
+                connectString: "(DESCRIPTION =(ADDRESS = (PROTOCOL = TCP)(HOST = oracle.cise.ufl.edu)(PORT = 1521))(CONNECT_DATA =(SID= ORCL)))"
+            })
+
+            const result = await connection.execute('SELECT YEAR, AVG(DISHWASH) AS Dishwashers FROM AHS GROUP BY YEAR ORDER BY YEAR ASC', [], { outFormat: oracledb.OUT_FORMAT_OBJECT })
+            return result;
+
+        } catch (error) {
+
+            console.error(error);
+            return error;
+
+        }
+
+    }
+
+    fetchDataStudents().then(dbRes => {
+
+        res.json(dbRes)
+
+    })
+        .catch(error => {
+
+            res.send("bru")
+
+        })
+
+    res.end;
+
+})
+
+app.get("/", (req, res) => {
+
+    res.send("Welcome to the backend")
+
 })
 
 app.listen(8080, () => { console.log("Server started on port 8080") })
 
-function MetroAreasQueries(x, y, z, h, stat){
+function MetroAreasQueries(x, y, z, h, stat) {
 
-    return ('select Vals_yearA.year, Vals_yearA.V1, Vals_yearB.V2, nat_indicators.'+y+' as Indicator'
-            + 'from' 
-            + '('
-            + 'select YEAR as year, TRUNC(AVG('+x+'),1) as V1' 
-            + 'from AHS'
-            + 'Where omb13cbsa = \'\'\''+z+'\'\'\''
-            + 'GROUP BY YEAR'
-            + 'ORDER BY YEAR ASC'
-            + ') Vals_yearA'
-            + 'JOIN' 
-            + 'NAT_INDICATORS on Vals_yearA.year = NAT_INDICATORS.YEAR'
-            + 'JOIN'
-            + '('
-            + 'select YEAR as year, TRUNC(AVG('+x+'),1) as V2' 
-            + 'from AHS'
-            + 'Where omb13cbsa = \'\'\''+h+'\'\'\''
-            + 'GROUP BY YEAR'
-            + 'ORDER BY YEAR ASC'
-            + ') Vals_yearB'
-            + 'on  Vals_yearB.year = NAT_INDICATORS.YEAR;')
+    return ('select Vals_yearA.year, Vals_yearA.V1, Vals_yearB.V2, nat_indicators.' + y + ' as Indicator'
+        + 'from'
+        + '('
+        + 'select YEAR as year, TRUNC(AVG(' + x + '),1) as V1'
+        + 'from AHS'
+        + 'Where omb13cbsa = \'\'\'' + z + '\'\'\''
+        + 'GROUP BY YEAR'
+        + 'ORDER BY YEAR ASC'
+        + ') Vals_yearA'
+        + 'JOIN'
+        + 'NAT_INDICATORS on Vals_yearA.year = NAT_INDICATORS.YEAR'
+        + 'JOIN'
+        + '('
+        + 'select YEAR as year, TRUNC(AVG(' + x + '),1) as V2'
+        + 'from AHS'
+        + 'Where omb13cbsa = \'\'\'' + h + '\'\'\''
+        + 'GROUP BY YEAR'
+        + 'ORDER BY YEAR ASC'
+        + ') Vals_yearB'
+        + 'on  Vals_yearB.year = NAT_INDICATORS.YEAR')
 }
-
-
 
 
 
 
 
 
-function metro(message){
 
-if(message == 'New York, NY'){
-    return '\'35620\''
-}
-if(message == 'Los Angeles, CA'){
-    return '\'31080\''
-}
-if(message == 'Chicago, IL'){
-    return '\'16980\''
-}
-if(message == 'Philadelphia, PA'){
-    return '\'19100\''
-}
-if(message == 'Huston, TX'){
-    return '\'37980\''
-}
-if(message == 'Washington, DC'){
-    return '\'26420\''
-}
-if(message == 'Miami, FL'){
-    return '\'47900\''
-}
-if(message == 'Atlanta, GA'){
-    return '\'33100\''
-}
-if(message == 'New York, NY'){
-    return '\'12060\''
-}
-if(message == 'Boston, MA'){
-    return '\'14460\''
-}
-if(message == 'San Francisco, CA'){
-    return '\'41860\''
-}
-if(message == 'Detroit, MI'){
-    return '\'19820\''
-}
-if(message == 'Phoenix, AZ'){
-    return '\'38060\''
-}
-if(message == 'Seattle, WA'){
-    return '\'35620\''
-}
-if(message == 'Riverside, CA'){
-    return '\'42660\''
-}
-else{
-    return '\'35620\''
-}
 
-}
+function metro(message) {
 
-function metroIndicator(message){
-
-if(message == 'Dish Washers'){
-    return 'DISHWASH'
-}
-if(message == 'Garages'){
-    return 'GARAGE'
-}
-if(message == 'Washing Machines'){
-    return 'WASHER'
-}
-if(message == 'Porch'){
-    return 'PORCH'
-}
-if(message == 'Kitchen Sink'){
-    return 'KITCHSINK'
-}
-if(message == 'Household Rating'){
-    return 'RATINGHS'
-}
-if(message == 'Neighborhood Rating'){
-    return 'RATINGNH'
-}
-if(message == 'Family Income'){
-    return 'FINCP'
-}
-if(message == 'Resident\'s Income'){
-    return 'HINCP'
-}
-if(message == 'Resident\'s Sex'){
-    return 'HHSEX'
-}
-if(message == 'Resident\'s Marriage Status'){
-    return 'HHMAR'
-}
-if(message == 'Resident\'s Education Level'){
-    return 'HHGRAD'
-}
-if(message == 'Resident\'s Age'){
-    return 'HHAGE'
-}
-if(message == 'Property Value'){
-    return 'MARKETVAL'
-}
-if(message == 'Monthly Mortgage'){
-    return 'MORTAMT'
-}
-if(message == 'Monthly Rent'){
-    return 'RENT'
-}
-if(message == 'Year Built'){
-    return 'YRBUILT'
-}
-if(message == 'Unit Size'){
-    return 'UNITSIZE'
-}
-if(message == 'No Access To Water'){
-    return 'NOWAT'
-}
-if(message == 'Monthly Home Expenses'){
-    return 'TOTHCAMT'
-}
-if(message == 'Total Rooms'){
-    return 'TOTROOM'
-}
-if(message == 'Rent Control Present'){
-    return 'RENTCNTRL'
-}
-if(message == 'Number of Inhabitants'){
-    return 'NUMPEOPLE'
-}
-else{
-    return 'RENT'
-}
+    if (message == 'New York, NY') {
+        return '\'35620\''
+    }
+    if (message == 'Los Angeles, CA') {
+        return '\'31080\''
+    }
+    if (message == 'Chicago, IL') {
+        return '\'16980\''
+    }
+    if (message == 'Philadelphia, PA') {
+        return '\'19100\''
+    }
+    if (message == 'Huston, TX') {
+        return '\'37980\''
+    }
+    if (message == 'Washington, DC') {
+        return '\'26420\''
+    }
+    if (message == 'Miami, FL') {
+        return '\'47900\''
+    }
+    if (message == 'Atlanta, GA') {
+        return '\'33100\''
+    }
+    if (message == 'New York, NY') {
+        return '\'12060\''
+    }
+    if (message == 'Boston, MA') {
+        return '\'14460\''
+    }
+    if (message == 'San Francisco, CA') {
+        return '\'41860\''
+    }
+    if (message == 'Detroit, MI') {
+        return '\'19820\''
+    }
+    if (message == 'Phoenix, AZ') {
+        return '\'38060\''
+    }
+    if (message == 'Seattle, WA') {
+        return '\'35620\''
+    }
+    if (message == 'Riverside, CA') {
+        return '\'42660\''
+    }
+    else {
+        return '\'35620\''
+    }
 
 }
 
+function metroIndicator(message) {
+
+    if (message == 'Dish Washers') {
+        return 'DISHWASH'
+    }
+    if (message == 'Garages') {
+        return 'GARAGE'
+    }
+    if (message == 'Washing Machines') {
+        return 'WASHER'
+    }
+    if (message == 'Porch') {
+        return 'PORCH'
+    }
+    if (message == 'Kitchen Sink') {
+        return 'KITCHSINK'
+    }
+    if (message == 'Household Rating') {
+        return 'RATINGHS'
+    }
+    if (message == 'Neighborhood Rating') {
+        return 'RATINGNH'
+    }
+    if (message == 'Family Income') {
+        return 'FINCP'
+    }
+    if (message == 'Resident\'s Income') {
+        return 'HINCP'
+    }
+    if (message == 'Resident\'s Sex') {
+        return 'HHSEX'
+    }
+    if (message == 'Resident\'s Marriage Status') {
+        return 'HHMAR'
+    }
+    if (message == 'Resident\'s Education Level') {
+        return 'HHGRAD'
+    }
+    if (message == 'Resident\'s Age') {
+        return 'HHAGE'
+    }
+    if (message == 'Property Value') {
+        return 'MARKETVAL'
+    }
+    if (message == 'Monthly Mortgage') {
+        return 'MORTAMT'
+    }
+    if (message == 'Monthly Rent') {
+        return 'RENT'
+    }
+    if (message == 'Year Built') {
+        return 'YRBUILT'
+    }
+    if (message == 'Unit Size') {
+        return 'UNITSIZE'
+    }
+    if (message == 'No Access To Water') {
+        return 'NOWAT'
+    }
+    if (message == 'Monthly Home Expenses') {
+        return 'TOTHCAMT'
+    }
+    if (message == 'Total Rooms') {
+        return 'TOTROOM'
+    }
+    if (message == 'Rent Control Present') {
+        return 'RENTCNTRL'
+    }
+    if (message == 'Number of Inhabitants') {
+        return 'NUMPEOPLE'
+    }
+    else {
+        return 'RENT'
+    }
+
+}
 
 
-function natIndicator(message){
 
-if(message == 'Inflation Rate'){
-    return 'INFLATION'
-}
-if(message == 'CCI'){
-    return 'CCI'
-}
-if(message == 'Federal Intrest Rate'){
-    return 'INTRATE'
-}
-if(message == 'GDP'){
-    return 'GDP'
-}
-if(message == 'Employment Rate'){
-    return 'EMPRATE'
-}
-if(message == 'Bond Yield'){
-    return 'BONDYIELD'
-}
-else{
-    return 'INFLATION'
-}
+function natIndicator(message) {
+
+    if (message == 'Inflation Rate') {
+        return 'INFLATION'
+    }
+    if (message == 'CCI') {
+        return 'CCI'
+    }
+    if (message == 'Federal Intrest Rate') {
+        return 'INTRATE'
+    }
+    if (message == 'GDP') {
+        return 'GDP'
+    }
+    if (message == 'Employment Rate') {
+        return 'EMPRATE'
+    }
+    if (message == 'Bond Yield') {
+        return 'BONDYIELD'
+    }
+    else {
+        return 'INFLATION'
+    }
 
 }
--- Frontend/cis-proj/src/components/MetroAreas.js
+++ Frontend/cis-proj/src/components/MetroAreas.js
@@ -81,23 +81,23 @@ export const data = {
 
 
 function MetropolitanAreas() {
-    const [metro1,setMetro1] = useState('');
-    const metroOne=(e)=> {
+    const [metro1, setMetro1] = useState('');
+    const metroOne = (e) => {
 
         console.log(e)
         setMetro1(e)
 
     }
 
-    const [metro2,setMetro2] = useState('');
-    const metroTwo=(e)=> {
+    const [metro2, setMetro2] = useState('');
+    const metroTwo = (e) => {
 
         console.log(e)
         setMetro2(e)
     }
 
-    const [nation,setNation] = useState('');
-    const nationChange=(e)=> {
+    const [nation, setNation] = useState('');
+    const nationChange = (e) => {
 
         console.log(e)
         setNation(e)
@@ -105,20 +105,20 @@ function MetropolitanAreas() {
 
     const submit = () => {
 
-        axios.post("http://localhost:8080/results",{ 
-            
+        axios.post("http://localhost:8080/results", {
+
             x: 'Household Rating',
             y: 'GDP',
             z: metro1,
             h: metro2,
             stat: 'mean'
-        
-        }
-        
-        )
-    
-      }
 
+        }
+
+        ).then(res => console.log(res.data.rows))
+
+    }
+    
     return (
         <div>
             <h1>Comparing Metropolitan Areas</h1>
@@ -153,21 +153,21 @@ function MetropolitanAreas() {
                 </Dropdown.Toggle>
 
                 <Dropdown.Menu>
-                <Dropdown.Item eventKey="New York, NY">New York, NY</Dropdown.Item>
-                <Dropdown.Item eventKey="Los Angeles, CA">Los Angeles, CA</Dropdown.Item>
-                <Dropdown.Item eventKey="Chicago, IL">Chicago, IL</Dropdown.Item>
-                <Dropdown.Item eventKey="Dallas, TX">Dallas, TX</Dropdown.Item>
-                <Dropdown.Item eventKey="Philadelphia, PA">Philadelphia, PA</Dropdown.Item>
-                <Dropdown.Item eventKey="Houston, TX">Houston, TX</Dropdown.Item>
-                <Dropdown.Item eventKey="Washington, DC">Washington, DC</Dropdown.Item>
-                <Dropdown.Item eventKey="Miami, FL">Miami, FL</Dropdown.Item>
-                <Dropdown.Item eventKey="Atlanta, GA">Atlanta, GA</Dropdown.Item>
-                <Dropdown.Item eventKey="Boston, MA">Boston, MA</Dropdown.Item>
-                <Dropdown.Item eventKey="San Francisco, CA">San Francisco, CA</Dropdown.Item>
-                <Dropdown.Item eventKey="Detroit, MI">Detroit, MI</Dropdown.Item>
-                <Dropdown.Item eventKey="Phoenix, AZ">Phoenix, AZ</Dropdown.Item>
-                <Dropdown.Item eventKey="Seattle, WA">Seattle, WA</Dropdown.Item>
-                <Dropdown.Item eventKey="Riverside, CA">Riverside, CA</Dropdown.Item>
+                    <Dropdown.Item eventKey="New York, NY">New York, NY</Dropdown.Item>
+                    <Dropdown.Item eventKey="Los Angeles, CA">Los Angeles, CA</Dropdown.Item>
+                    <Dropdown.Item eventKey="Chicago, IL">Chicago, IL</Dropdown.Item>
+                    <Dropdown.Item eventKey="Dallas, TX">Dallas, TX</Dropdown.Item>
+                    <Dropdown.Item eventKey="Philadelphia, PA">Philadelphia, PA</Dropdown.Item>
+                    <Dropdown.Item eventKey="Houston, TX">Houston, TX</Dropdown.Item>
+                    <Dropdown.Item eventKey="Washington, DC">Washington, DC</Dropdown.Item>
+                    <Dropdown.Item eventKey="Miami, FL">Miami, FL</Dropdown.Item>
+                    <Dropdown.Item eventKey="Atlanta, GA">Atlanta, GA</Dropdown.Item>
+                    <Dropdown.Item eventKey="Boston, MA">Boston, MA</Dropdown.Item>
+                    <Dropdown.Item eventKey="San Francisco, CA">San Francisco, CA</Dropdown.Item>
+                    <Dropdown.Item eventKey="Detroit, MI">Detroit, MI</Dropdown.Item>
+                    <Dropdown.Item eventKey="Phoenix, AZ">Phoenix, AZ</Dropdown.Item>
+                    <Dropdown.Item eventKey="Seattle, WA">Seattle, WA</Dropdown.Item>
+                    <Dropdown.Item eventKey="Riverside, CA">Riverside, CA</Dropdown.Item>
                 </Dropdown.Menu>
             </Dropdown>
 
