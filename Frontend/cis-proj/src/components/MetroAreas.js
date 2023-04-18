67fd4021588823ffd4fc760199c97dd9d859270a
ManuelNunez08 <Manuel.Nunez@ufl.edu>
2023-04-18 12:46:48 -0400

    minor

--- Frontend/cis-proj/src/components/MetroAreas.js
+++ Frontend/cis-proj/src/components/MetroAreas.js
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import axios from 'axios'
 import Button from 'react-bootstrap/Button'
+import { LineChartComp } from './LineChartComp';
 
 
 function MetropolitanAreas() {
